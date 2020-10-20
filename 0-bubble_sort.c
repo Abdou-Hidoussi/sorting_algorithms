88abecb13fcabd5e15d2c12becd299a99463272a
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-20 19:50:17 +0000

    change

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,9 +7,9 @@
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
-	size_t i;
+	size_t i, j;
 
-	for (i = 0; i < size; i++)
+	for (j = 0; j < size; j++)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
