f4185974764c3aa48a593a09bc18f5b8c1a5d8a9
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-10-20 19:47:56 +0000

    change

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@ void bubble_sort(int *array, size_t size)
 	int tmp;
 	size_t i;
 
-	while (is_sorted(array, size))
+	for (i = 0; i < size - 1; i++)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
@@ -22,23 +22,4 @@ void bubble_sort(int *array, size_t size)
 			print_array(array, size);
 		}
 	}
-}
-/**
- * is_sorted - check if array sorted
- * @array: int
- * @size: size_t
- * Return: 1 or 0
- */
-int is_sorted(int *array, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		if (array[i] > array[i + 1])
-		{
-			return (1);
-		}
-	}
-	return (0);
 }
